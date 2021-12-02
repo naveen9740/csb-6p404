afc78b31fb564258599de8cda2fe5d30325694df
Code Sandbox <hello@codesandbox.io>
2021-12-02 17:04:08 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.df2800bdfff5d3100472533064e9d857.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "296072bb6eb3e54858beb38f9d118f6c",
+    "url": "https://naveen9740.github.io/csb-6p404/index.html"
+  },
+  {
+    "revision": "62f9f21e5b3adf50d60c",
+    "url": "https://naveen9740.github.io/csb-6p404/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "98a18730b6272c6821f9",
+    "url": "https://naveen9740.github.io/csb-6p404/static/js/2.093dd12b.chunk.js"
+  },
+  {
+    "revision": "354425ea9be9e856f60fa8ca4fc34c01",
+    "url": "https://naveen9740.github.io/csb-6p404/static/js/2.093dd12b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62f9f21e5b3adf50d60c",
+    "url": "https://naveen9740.github.io/csb-6p404/static/js/main.595d6a57.chunk.js"
+  },
+  {
+    "revision": "9ae21432861d064113a3",
+    "url": "https://naveen9740.github.io/csb-6p404/static/js/runtime-main.ad9ffe7e.js"
+  }
+]);
